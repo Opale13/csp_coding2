6bdbaa4e37e3a5422170aecc0d5c3a4d92b4fefc
WhiteShadow13 <ludo.m@live.fr>
2020-04-17 11:50:40 +0200

    Add find high degree

--- tree.py
+++ tree.py
@@ -24,19 +24,43 @@ class Node():
         return all(v.is_assigned() for v in self.variables) and all(c.is_satisfied() for c in self.constraints)
 
 
+    def _find_variable_with_high_degree(self):
+        var = dict()
+
+        for variable in self.variables:
+            if variable.value == None:
+                var[variable.name] = 0
+                for constraint in self.constraints:
+                    if variable in constraint.variables:
+                        var[variable.name] += 1
+        
+        return max(var, key=var.get)
+
+
     def take_decision(self):        
         if self.layer < len(self.variables) and self._check_constraints():
-            for dom in self.variables[self.layer].domain:
-                self.variables[self.layer].value = dom
-                self.variables[self.layer].domain.remove(dom)
+            #first variable
+            # variable = self.variables[self.layer]
+
+            #high degree
+            var = self._find_variable_with_high_degree()
+
+            variable = None
+            for v in self.variables:
+                if v.name == var:
+                    variable = v
+
+            for dom in variable.domain:
+                variable.value = dom
+                variable.domain.remove(dom)
                 next_node = Node(self.variables, self.constraints, self.layer+1)
 
                 if next_node.take_decision():
                     return True
                 
                 else:
-                    self.variables[self.layer].value = None
-                    self.variables[self.layer].domain.insert(dom, dom)
+                    variable.value = None
+                    variable.domain.insert(dom, dom)
             
             return False
                 
